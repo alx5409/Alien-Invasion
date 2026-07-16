e2dc6fc61791117540b2fb07db7b6cd46dbbc932
alx5409 <alx5409@gmail.com>
2026-07-16 23:52:34 +0200

    Fix score board types with None rect

--- alien_invasion/scoreboard.py
+++ alien_invasion/scoreboard.py
@@ -75,6 +75,8 @@ class Scoreboard:
         self.ships = Group()
         for ship_number in range(self.stats.ships_left):
             ship = Ship(self.ai_game)
+            if not ship.rect:
+                raise ValueError("Ship rect is not initialized.")
             ship.rect.x = 10 + ship_number + ship.rect.width
             ship.rect.y = 10
             self.ships.add(ship)
